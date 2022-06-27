0f78abc335edbeb6ea3b68c1cd24baaf93ec13c6
User <User@DESKTOP-HNQRK10>
2022-06-27 20:00:56 +0300

    Добавьте файлы проекта.

---
+++ EX1/Building.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public interface rent
+{
+    public int Rent(int price, int quantity, int days);
+}
+
+public interface ticket
+{
+    public int Price(int price, int customer_age);
+}
+
+public abstract class Building
+{
+    public string Date { get; set; }
+    public int Square { get; set; }
+    public int Floors { get; set; }
+    public Building(string date, int square, int floors)
+    {
+        this.Date = date;
+        this.Square = square;
+        this.Floors = floors;
+    }
+
+    public abstract int capacity();
+}
+
+public class PublicBuilding : Building, ticket
+{
+
+    public PublicBuilding(string date, int square, int floors) : base(date, square, floors) { }
+
+    public int Price(int price, int customer_age)
+    {
+       
+        return price;
+    }
+
+    public override int capacity()
+    {
+        return (this.Square * this.Floors) / 5;
+    }
+}
+
+public class ResidentialBuilding : Building, rent
+{
+    public ResidentialBuilding(string date, int square, int floors) : base(date, square, floors) { }
+
+    public int Rent(int price_per_meter, int square, int days)
+    {
+        return price_per_meter * square * days;
+    }
+
+    public override int capacity()
+    {
+        return (this.Square * this.Floors) / 8;
+    }
+}
+
+public class Cinema : PublicBuilding, ticket
+{
+    public Cinema(string date, int square, int floors) : base(date, square, floors) { }
+    public int Price { get; set; }
+
+  
+
+    public override int capacity()
+    {
+        return (this.Square * this.Floors) / 4;
+    }
+
+}
+
+public class Hotel : ResidentialBuilding, rent
+{
+    public Hotel(string date, int square, int floors) : base(date, square, floors) { }
+
+    public override int capacity()
+    {
+        return (this.Square * this.Floors) / 7;
+    }
+}
+
+public class City
+{
+    public List<Building> Buildings;
+    public City(List<Building> building_list)
+    {
+        this.Buildings = building_list;
+    }
+
+    public void add_building(Building building)
+    {
+        Buildings.Add(building);
+    }
+
+    public void show_building()
+    {
+        foreach (Building b in Buildings)
+        {
+            Console.WriteLine(b);
+        }
+    }
+
+    public void average_capacity()
+    {
+        int cinema_sum = 0;
+        int cinema_count = 0;
+        int hotel_sum = 0;
+        int hotel_count = 0;
+        int public_sum = 0;
+        int public_count = 0;
+        int residental_sum = 0;
+        int residental_count = 0;
+        foreach (Building b in Buildings)
+        {
+            if (b is Cinema)
+            {
+                cinema_sum += b.capacity();
+                cinema_count += 1;
+            }
+            if (b is Hotel)
+            {
+                hotel_sum += b.capacity();
+                hotel_count += 1;
+            }
+
+            if (b is PublicBuilding)
+            {
+                public_sum += b.capacity();
+                public_count += 1;
+            }
+
+            if (b is ResidentialBuilding)
+            {
+                residental_sum += b.capacity();
+                residental_count += 1;
+            }
+
+        }
+        
+    }
+}
---
+++ EX1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cinema florencia = new Cinema("07.03.2012", 100, 5);
+            Console.WriteLine(florencia);
+
+            Console.WriteLine("Built: " + florencia.Date);
+
+            Console.WriteLine("Ticket price: " + florencia.Price(100, 3));   
+            
+            Console.WriteLine("Сapacity: " + florencia.capacity());
+
+            ////////////////////////////////////////////////////////////////////
+
+            Hotel California = new Hotel("21.08.2004", 310, 8);
+
+            Console.WriteLine(California);
+
+            Console.WriteLine("Built: " + California.Date);
+
+            Console.WriteLine("Rent price: " + California.Rent(100, 3, 10));
+
+            Console.WriteLine("Сapacity: " + California.capacity());
+
+            ////////////////////////////////////////////////////////////////////
+
+            PublicBuilding publickbuilding1 = new PublicBuilding("23.08.1923", 200, 4);
+
+            Console.WriteLine(publickbuilding1);
+
+            Console.WriteLine("Built: " + publickbuilding1.Date);
+
+            Console.WriteLine("Ticket price: " + publickbuilding1.Price(50, 34));
+
+            Console.WriteLine("Сapacity: " + publickbuilding1.capacity());
+
+            /////////////////////////////////////////////////////////////////////
+
+            ResidentialBuilding house = new ResidentialBuilding("01.01.2012", 150, 20);
+
+            Console.WriteLine(house);
+
+            Console.WriteLine("Built: " + house.Date);
+
+            Console.WriteLine("Rent price: " + house.Rent(150, 50, 30));
+
+            Console.WriteLine("Сapacity: " + house.capacity());
+
+            /////////////////////////////////////////////////////////////////////
+
+
+            
+        }
+    }
+}
